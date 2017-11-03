c74ce1411541b1bc85f16d8b177095b16f044501
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-03 15:11:27 +0900

    init commit

---
+++ lib/pghub/assign.rb
@@ -0,0 +1,7 @@
+require "pghub/assign/version"
+
+module Pghub
+  module Assign
+    # Your code goes here...
+  end
+end
---
+++ spec/pghub/assign_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Pghub::Assign do
+  it "has a version number" do
+    expect(Pghub::Assign::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
