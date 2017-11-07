0e869569d7da20194e70d15ac794c0c3f37877f9
ebkn12 <ktennis.mqekr12@gmail.com>
2017-11-07 15:02:44 +0900

    implemention in one file

--- lib/pghub/assign.rb
+++ lib/pghub/assign.rb
@@ -1,15 +1,102 @@
 require 'pghub/assign/version'
 require 'pghub/base'
-require 'pghub/github_api_assign'
 
-include GithubAPIAssign
+include GithubAPI::Connection
 
 module Pghub
   module Assign
     class << self
-      def assign(issue_path)
-        assign_client = GithubAPIAssign.new(issue_path)
-        assign_client.assign
+      def post(issue_path, opened_user)
+        all_members = {}
+        all_teams_data.each do |team|
+          all_members[team['name'].to_s] = all_members_from(team['id'])
+        end
+
+        # TODO: define method for this process
+        all_members.keys do |team_name|
+          raise 'unknown team name' unless team_name.include?(Pghub.config.assign_numbers.keys)
+        end
+
+        selected_assignees = assignees(all_members, opened_user)
+        assign(issue_path, selected_assignees)
+
+        selected_reviewers = reviewers(selected_assignees, opened_user)
+        review_request(issue_path, selected_reviewers)
+      end
+
+      private
+
+      # get team_id
+      # return [{ "hoge" => "43525" }, { "hoge" => "52431"} ...]
+      def all_teams_data
+        response = connection.get("/orgs/#{Pghub.config.github_organization}/teams?access_token=#{Pghub.config.github_access_token}")
+        body = JSON.parse(response.body)
+
+        # TODO: rename
+        body.map do |h|
+          { 'id' => h['id'], 'name' => h['name'] }
+        end
+      end
+
+      # get members related to teams from github
+      # return { "team_name": ["member_name", "member_name"...] }
+      def all_members_from(team_id)
+        response = connection.get("/teams/#{team_id}/members?access_token=#{Pghub.config.github_access_token}")
+        body = JSON.parse(response.body)
+
+        body.map { |h| h['login'] }
+      end
+
+      # selected assignees
+      # return ["member_name", "member_name"]
+      def assignees(all_members, opened_user)
+        target_team_numbers = Pghub.config.assign_numbers
+        assignees = [opened_user]
+
+        target_team_numbers.each do |team, num_of_members|
+          members = all_members[team.to_s]
+
+          raise 'too many assign_numbers' if num_of_members > members.length
+
+          if all_members[team.to_s].include?(opened_user)
+            members.delete(opened_user)
+            num_of_members -= 1
+          end
+
+          num_of_members.times do
+            selected_member = members.sample
+            members.delete(selected_member)
+            assignees << selected_member unless assignees.include?(selected_member)
+          end
+        end
+
+        assignees
+      end
+
+      # selected reviewers (except a member who opened PR)
+      def reviewers(assignees, opened_user)
+        assignees.delete(opened_user)
+        assignees
+      end
+
+      # post assign data
+      def assign(issue_path, assignees)
+        connection.post do |req|
+          req.url "/repos/#{Pghub.config.github_organization}/#{issue_path}/assignees?access_token=#{Pghub.config.github_access_token}"
+          req.headers['Content-Type'] = 'application/json'
+          req.body = { assignees: assignees }.to_json
+        end
+      end
+
+      # post review request data
+      def review_request(issue_path, reviewers)
+        pr_path = issue_path.gsub('issues', 'pulls')
+
+        connection.post do |req|
+          req.url "/repos/#{Pghub.config.github_organization}/#{pr_path}/requested_reviewers?access_token=#{Pghub.config.github_access_token}"
+          req.headers['Content-Type'] = 'application/json'
+          req.body = { reviewers: reviewers }.to_json
+        end
       end
     end
   end
